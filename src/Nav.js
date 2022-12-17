55933bc34e29064987b11c13a1c460c0e035e5c1
Mariam <106632629+MariamHesham21@users.noreply.github.com>
2022-12-17 21:13:40 +0200

    Add files via upload

--- src/Nav.js
+++ src/Nav.js
@@ -3,22 +3,24 @@ import { BsCartFill,BsSearch } from "react-icons/bs";
 import {Link} from 'react-router-dom'
 import ShopingCart from './Cart/ShopingCart';
 
-const Nav = ({size,Search}) => {
+const Nav = ({size,Search,setWord}) => {
 
   return (
     <div className='Nav'>
         <Link to='/'><img src='https://res.cloudinary.com/sivadass/image/upload/v1493547373/dummy-logo/Veggy.png' alt='img-ve' className='logo'/></Link>
-        <div className='search'>
-            <input type='search' placeholder='seach for vegetables and fruits...' />
+        <div className='sear-car'> 
+          <div className='search'>
+            <input type='search' placeholder='seach for vegetables and fruits...' onChange={(e)=>setWord(e.target.value)} className='input-search'/>
             <button className="search-button" type="submit" >
                 <BsSearch className='icon-search'/>
             </button>
-        </div>
-        <div className='Cart-shopping'>
+          </div>
+          <div className='Cart-shopping'>
            <Link to='/ShopingCart'>
            <BsCartFill className='icon-cart' />
            </Link>
             <span className='change-number'>{size}</span>
+          </div>
         </div>
 
     </div>
